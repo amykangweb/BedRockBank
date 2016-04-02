793a1916bb327cf9491267bfb363399eb7003409
amykangweb <selene6023@gmail.com>
2016-04-02 11:50:35 -0700

    Initial codebase.

---
+++ BedrockBank/Account.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockBank
+{
+    public enum AccountType
+    {
+        Savings,
+        Checking,
+        CD,
+        Loans
+    }
+    /// <summary>
+    /// This is about a bank account
+    /// for the customer
+    /// </summary>
+    public class Account
+    {
+        #region Variables
+        private static int lastAccountNumber = 0;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Account number for the account
+        /// </summary>
+        public int AccountNumber { get; private set; }
+        /// <summary>
+        /// Name of the account
+        /// </summary>
+        public string AccountName { get; set; }
+        public int SSN { get; set; }
+        public double Balance { get; private set; }
+        public AccountType TypeOfAccount { get; set; }
+
+        #endregion
+
+        #region Constructor
+        public Account()
+        {
+            //lastAccountNumber = lastAccountNumber + 1;
+            //AccountNumber = lastAccountNumber;
+            AccountNumber = ++lastAccountNumber;
+        }
+        #endregion
+
+        #region Methods
+        public double Deposit(double amount)
+        {
+            Balance += amount;
+            return Balance;
+        }
+        #endregion
+    }
+}
---
+++ BedrockBank/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an instance of Account
+            var account1 = new Account();
+            account1.AccountName = "My checking";
+            account1.TypeOfAccount = AccountType.Checking;
+            var newBalance = account1.Deposit(300.00);
+            Console.WriteLine("Account Name: {0}, Account Number: {1}, Type of account: {2}, Balance: {3:c}", 
+                account1.AccountName, account1.AccountNumber, account1.TypeOfAccount, account1.Balance);
+
+            // Creating an instance of Account
+            var account2 = new Account();
+            account2.AccountName = "My savings";
+            account2.TypeOfAccount = AccountType.Savings;
+            account2.Deposit(200.00);
+            Console.WriteLine("Account Name: {0}, Account Number: {1}, Type of account: {2}, Balance: {3:c}",
+                account2.AccountName, account2.AccountNumber, account2.TypeOfAccount, account2.Balance);
+        }
+    }
+}
